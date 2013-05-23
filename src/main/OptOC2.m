c57f83c08d48383eefb125200631d701515b7de2
sehlstrom <alexander@sehlstrom.se>
2013-05-23 16:54:46 +0200

    Update OptOC2.m
    Now takes a struct as input representing the strucutre that is supposed
    to be optimized.

--- src/main/OptOC2.m
+++ src/main/OptOC2.m
@@ -1,4 +1,4 @@
-function [ x, k ] = OptOC2( x0, xp, ep, Edof, bc, Ex, Ey, F, Ke0, OFun, vfrac, ip, ft, d )
+function [ x, k ] = OptOC2( s, OFun, vfrac, ip, ft, d, varargin )
 %OPTOC2 Topology optimization accoridng to the optimality cirteria (OC)
 %method. Mesh consists of equal size square 2D elements.
 %
@@ -39,42 +39,43 @@ function [ x, k ] = OptOC2( x0, xp, ep, Edof, bc, Ex, Ey, F, Ke0, OFun, vfrac, i
 %   of the first order derivative of the objective.
 %
 %   INPUT ARGUMENTS
-%       x0     vector of initial guesses for the design parameters;
-%              0 <= x0 <= 1
-%       xp     prescribed parameters; one row [e, xp] for each element e
-%              which has a prescribed value xp for it's design variable xe.
-%              The values are given within the range 0 <= xp <= 1. If no
-%              parameters are to be prescribed
-%       ep     element properties; ep = [E0, Emin, t, l] where
-%              E0       Young's modulus of the base material [Pa]
-%              Emin     minimum Young's modulus [Pa]
-%              t        element thickness [m]
-%              l        element side length [m]
-%       Ke0    element stiffness matrix with unit stiffness (E=1)
-%       Me0    element mass matrix with unit mass (m=1)
-%       Edof   elemnet degrees of freedom; one row for each element
-%              [u1,u2,u3,...,un]
-%       Ex     element x-coordinates; one row for each element
-%              [ex1,ex2,...,exn]
-%       Ey     element y-coordinates; one row for each element
-%              [ey1,ey2,...,eyn]
-%       F      vector of loads
-%       vfrac  volume fraction constraint; 0 < vfrac < 1
-%       ip     (optional) cell array with interpolation function and
-%              related interpolation parameter; ip = {@EFun, param}.
-%              If ip is not set, the ELin interplation function will be
-%              used.
-%       ft     (optional) cell array with filter function and related
-%              filter parameter; ft = {@FFun, rmin}
-%       d      (optional) cell array with display settings;
-%              d = {print, plot, figure}
-%              print    1 - if each iteration should print it's result
-%                       0 - if no print should be done
-%              plot     1 - if the structure should be plotted in each
-%                           iteration
-%                       0 - if the structure should not be plotted in each
-%                           iteration
-%              figure   figure handle that should be used when plotting
+%   s      a struct with at least the following fields
+%          x0     vector of initial guesses for the design parameters;
+%                     0 <= x0 <= 1
+%          xp     prescribed parameters; one row [e, xp] for each element
+%                 e which has a prescribed value xp for it's design
+%                 variable xe. The values are given within the range
+%                 0 <= xp <= 1. If no parameters are to be prescribed
+%          ep     element properties; ep = [E0, Emin, t, l] where
+%                 E0       Young's modulus of the base material [Pa]
+%                 Emin     minimum Young's modulus [Pa]
+%                 t        element thickness [m]
+%                 l        element side length [m]
+%          Ke0    element stiffness matrix with unit stiffness (E=1)
+%          Me0    element mass matrix with unit mass (m=1)
+%          Edof   elemnet degrees of freedom; one row for each element
+%                 [u1,u2,u3,...,un]
+%          Ex     element x-coordinates; one row for each element
+%                 [ex1,ex2,...,exn]
+%          Ey     element y-coordinates; one row for each element
+%                 [ey1,ey2,...,eyn]
+%          F      vector of loads
+%   OFun   objective function
+%   vfrac  volume fraction constraint; 0 < vfrac < 1
+%   ip     (optional) cell array with interpolation function and
+%          related interpolation parameter; ip = {@EFun, param}.
+%          If ip is not set, the ELin interplation function will be used.
+%   ft     (optional) cell array with filter function and related
+%          filter parameter; ft = {@FFun, rmin}
+%   d      (optional) cell array with display settings;
+%          d = {print, plot, figure}
+%          print    1 - if each iteration should print it's result
+%                   0 - if no print should be done
+%          plot     1 - if the structure should be plotted in each
+%                   iteration
+%                   0 - if the structure should not be plotted in each
+%                   iteration
+%          figure   figure handle that should be used when plotting
 %
 %   OUTPUT ARGUMENTS
 %       x      optimized design parameters
@@ -85,69 +86,95 @@ function [ x, k ] = OptOC2( x0, xp, ep, Edof, bc, Ex, Ey, F, Ke0, OFun, vfrac, i
 % LAST MODIFIED: A Sehlstrom    2013-05-23
 % Copyright (C)  A Sehlstrom
 
+% Parse inputs ------------------------------------------------------------
+parseo = inputParser;
+addRequired(parseo,'s', @isstruct);
+addRequired(parseo,'OFun');
+addRequired(parseo,'vfrac', @(x) isscalar(x) && x >= 0 && x <= 1);
+
+addOptional(parseo,'ip', {@ELin, []});
+addOptional(parseo,'ft', {});
+addOptional(parseo,'d',  {0, 0, []});
+
+addParamValue(parseo,'maxIter', 50,   @isinteger);
+addParamValue(parseo,'absTol',  1e-2, @isscalar);
+addParamValue(parseo,'gradTol', 1e-6, @isscalar);
+addParamValue(parseo,'lTol',    1e-4, @isscalar); % Tolerance for lagrangian multiplier
+addParamValue(parseo,'move',    0.2,  @isscalar);  % Move limit for update of x
+addParamValue(parseo,'eta',     0.5,  @isscalar);  % Numerical damping parameter
+
+parseo.parse(s, OFun, vfrac, ip, ft, d, varargin{:});
+
+% Extract
+stop_maxIter = parseo.Results.maxIter;
+stop_absTol  = parseo.Results.absTol;
+stop_gradTol = parseo.Results.gradTol;
+
+oc_lTol = parseo.Results.lTol;
+oc_move = parseo.Results.move;
+oc_eta  = parseo.Results.eta;
+
 % Input checks ------------------------------------------------------------
 % Design parameters x
-if size(x0,2) ~= 1
+if size(s.x,2) ~= 1
     error('OptOC2:argChk', '"x0" must be a row vector');
 else
-    nelem = size(x0, 1);
+    nelem = size(s.x, 1);
 end
 
 % Prescribed design paramters x
-if size(xp) ~= [0,0]
-    felem = setdiff(1:nelem, xp(:,1));
+if size(s.xp) ~= [0,0]
+    felem = setdiff(1:nelem, s.xp(:,1));
 else
     felem = 1:nelem;
 end
 
-% Element properties
-ep(1,:) = ep;
-if size(ep, 2) ~= 4
-    error('OptOC2:argChk', '"ep" has to have 4 elements');
-else
-    E0   = ep(:,1);
-    Emin = ep(:,2);
-    t    = ep(:,3);
-    l    = ep(:,4);
-end
-
 % Unit stiffness matrix
-if size(Ke0,1) ~= size(Ke0,2)
+if isnan(s.Ke0)
+    error('OptOC2:argChk', '"Ke0" has to be specified in struct "s"');
+elseif size(s.Ke0,1) ~= size(s.Ke0,2)
     error('OptOC2:argChk', '"Ke0" has to be square');
 else
-    ndofe = size(Ke0,1);
+    ndofe = size(s.Ke0,1);
 end
 
 % Element degrees of freedom
-if size(Edof, 1) ~= nelem
+if isnan(s.Edof)
+    error('OptOC2:argChk', '"Edof" has to be specified in struct "s"');
+elseif size(s.Edof, 1) ~= nelem
     error('OptOC2:argChk', '"Edof" has to have nelem rows');
-elseif size(Edof, 2) ~= ndofe
+elseif size(s.Edof, 2) ~= ndofe
     error('OptOC2:argChk', '"Edof" has to have ndofe columns');
 else
-    ndof = max(max(Edof));
+    ndof = max(max(s.Edof));
 end
 
-
 % Boundary conditions
-if size(bc,1) == 0
+if size(s.bc,1) == 0
     error('OptOC2:argChk','"bc" has to have at least 1 row')
-elseif size(bc, 2) ~= 2
+elseif size(s.bc, 2) ~= 2
     error('OptOC2:argChk','"bc" has to have 2 columns')
 else
-    fdof = setdiff(1:ndof, bc(:,1));
+    fdof = setdiff(1:ndof, s.bc(:,1));
 end
 
 % Element coordinates
-if size(Ex) ~= size(Ey)
+if isnan(s.Ex)
+    error('OptOC2:argChk', '"Ex" has to be specified in struct "s"');
+elseif isnan(s.Ey)
+    error('OptOC2:argChk', '"Ey" has to be specified in struct "s"');
+elseif size(s.Ex) ~= size(s.Ey)
     error('OptOC2:argChk', '"Ex" and "Ey" has to have the same size');
-elseif size(Ex, 1) ~= nelem
+elseif size(s.Ex, 1) ~= nelem
     error('OptOC2:argChk', '"Ex" and "Ey" has to have nelem rows');
 end
 
 % Load vector
-if size(F,1) ~= ndof
+if isnan(s.F)
+    error('OptOC2:argChk', '"F" has to be specified in struct "s"');
+elseif size(s.F,1) ~= ndof
     error('OptOC2:argChk', '"F" must have same number of degrees of freedom as specified in "Edof"');
-elseif size(F,2) ~= 1
+elseif size(s.F,2) ~= 1
     error('OptOC2:argChk', '"F" must be a row vector');
 end
 
@@ -162,35 +189,26 @@ end
 
 % Additional functions ----------------------------------------------------
 % Young's modulus by interpolation setup
-if exist('ip','var')
-    interp_fun   = ip{1};
-    interp_param = ip{2};
-else
-    interp_fun   = @ELin;
-    interp_param = [];
-end
+
+interp_fun   = ip{1};
+interp_param = ip{2};
 
 % Filter setup
-if exist('ft','var')
+if size(ft, 1) ~= 0
     filter_fun  = ft{1};
     filter_name = func2str(filter_fun);
     filter_rmin = ft{2};
-    ec = [(Ex(:,1)+Ex(:,2))/2, (Ey(:,1)+Ey(:,3))/2];
-    [filter_H, filter_Hs] = FUsetup(ec, filter_rmin);
+    ec = [(s.Ex(:,1)+s.Ex(:,2))/2, (s.Ey(:,1)+s.Ey(:,3))/2];
+    [filter_H, filter_Hs] = UFilterSetup(ec, filter_rmin);
 end
 
 % Display setup
-if exist('d','var')
-    display_print  = d{1};
-    display_plot   = d{2};
-    display_figure = d{3};
-else
-    display_print = 0;
-    display_plot  = 0;
-end
+display_print  = d{1};
+display_plot   = d{2};
+display_figure = d{3};
 
 % Initial guess -----------------------------------------------------------
-xk = x0;
+xk = s.x;
 
 % Plot initial guess if required
 if display_plot
@@ -199,7 +217,7 @@ if display_plot
     hold on;
     colormap(flipud(gray));
     for ii = 1:nelem
-        fill(Ex(ii,:), Ey(ii,:), xk(ii));
+        fill(s.Ex(ii,:), s.Ey(ii,:), xk(ii));
     end
     caxis([0 1]);
     colorbar;
@@ -210,26 +228,12 @@ if display_plot
 end
 
 % Constraint --------------------------------------------------------------
-V0     = l*l*t;
-VOmega = nelem*V0;
-dV     = ones(length(x0),1)*V0;
+dV  = ones(nelem,1)*s.Ve0;
 
 % Filter volume derivatives if needed
 if exist('filter_name','var') && strcmp(filter_name,'FDensity')
     [~, dV]    = filter_fun(filter_H, filter_Hs, xk, dV);
 end
-
-% Limits ------------------------------------------------------------------
-% TODO: Make it possible to change these values via the function call
-% Stop criteria limits
-stop_maxIter = 50;
-stop_absTol  = 1e-3;
-stop_gradTol = 1e-6;
-
-% Optimality criteria method settings
-oc_ltol    = 1e-4;      % Tolerance for lagrangian multiplier
-oc_move    = 0.2;       % Move limit for update of x
-oc_eta     = 0.5;       % Numerical damping parameter
 
 % Optimize ----------------------------------------------------------------
 k = 0;
@@ -238,16 +242,17 @@ while 1
     xold = xk;
     
     % INTERPOLATE MATERIAL PROPERTIES
-    [E, dE] = interp_fun(xk, E0, Emin, interp_param);
+    [s.E, s.dE] = interp_fun(xk, s.E0, s.Emin, interp_param);
     
     % ASSEMBLE STIFFNESS MATRIX
-    K = zeros(ndof, ndof);
-    for ii = 1:length(x0)
-        K(Edof(ii,:),Edof(ii,:)) = K(Edof(ii,:),Edof(ii,:)) + E(ii)*Ke0;
+    s.K = zeros(ndof, ndof);
+    for ii = 1:nelem
+        s.K(s.Edof(ii,:),s.Edof(ii,:)) = s.K(s.Edof(ii,:),s.Edof(ii,:)) + s.E(ii)*s.Ke0;
     end
     
     % OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS
-    [O, dO] = OFun(xk, xp, Edof, bc, F, K, Ke0, E, dE);
+    s.x = xk;
+    [O, dO] = OFun(s);
     
     % APPLY FILTER
     if exist('filter_name','var')
@@ -257,7 +262,7 @@ while 1
     % DESIGN UPDATE BY THE OPTIMALITY CRITERIA METHOD
     l1 = 0; l2 = 100000;
     fac = max(0, -dO)./dV;
-    while (l2-l1 > oc_ltol)
+    while (l2-l1 > oc_lTol)
         % Lagrangian midvalue guess
         lmid = 0.5*(l2+l1);
         
@@ -265,8 +270,8 @@ while 1
         x_new = max(0,max(xk-oc_move,min(1.,min(xk+oc_move,xk.*(fac/lmid).^oc_eta))));
         
         % Set prescribed values
-        if size(xp) ~= [0,0]
-            x_new(xp(:,1)) = xp(:,2);
+        if size(s.xp) ~= [0,0]
+            x_new(s.xp(:,1)) = s.xp(:,2);
         end
         
         % Apply filter on x if needed
@@ -275,12 +280,12 @@ while 1
         end
         
         % Set prescribed values
-        if size(xp) ~= [0,0]
-            x_phys(xp(:,1)) = xp(:,2);
+        if size(s.xp) ~= [0,0]
+            x_phys(s.xp(:,1)) = s.xp(:,2);
         end
         
         % Test optimality
-        if sum(sum(x_phys*V0)) - vfrac*VOmega > 0;
+        if sum(sum(x_phys*s.Ve0)) - vfrac*s.V0 > 0;
             l1 = lmid;
         else
             l2 = lmid;
@@ -296,7 +301,7 @@ while 1
         hold on;
         colormap(flipud(gray));
         for ii = 1:nelem
-            fill(Ex(ii,:), Ey(ii,:), xk(ii));
+            fill(s.Ex(ii,:), s.Ey(ii,:), xk(ii));
         end
         caxis([0 1]);
         colorbar;
@@ -315,8 +320,8 @@ while 1
     
     % Print
     if display_print
-        V = sum(xk*V0,1);
-        fprintf('It.: %4i Obj.: %3.3e Vol.frac.: %1.2f Ch.: %3.3e N.: %3.3e \n', k, O, V/VOmega, c, n);
+        V = sum(xk*s.Ve0,1);
+        fprintf('It.: %4i Obj.: %3.3e Vol.frac.: %1.2f Ch.: %3.3e N.: %3.3e \n', k, O, V/s.V0, c, n);
     end
     
     % STOPPING CRITERIA
